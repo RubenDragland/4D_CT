78f1c38feaa150403aac50b28902be245ace68fe
Ruben Dragland <ruben.s.dragland@gmail.com>
2023-04-12 14:23:08 +0200

    Augmentation and cropping

--- 3DTomoGAN/data.py
+++ 3DTomoGAN/data.py
@@ -7,25 +7,80 @@ import torch
 # from torchvision.io import read_image
 import imageio
 from torch.utils.data import Dataset
+import torchio as tio
 
 # import torchvision.transforms.functional as TF
 import torchvision.transforms as transforms
 import h5py
 
-# RSD: Ish initialised.
+
+def inverse_transform(tensor):
+    return 1 - tensor.copy()
 
 
-# RSD: Need plan for cropping etc. Do this as target transform and transform? Possibly.
+def random_inverse_transform(tensor, p=0.5):
+    if np.random.random() > p:
+        return inverse_transform(tensor)
+    else:
+        return tensor
 
-# RSD: From h5 to dataset is the plan.
+
+inverse_transform = tio.Lambda(inverse_transform)
+random_inverse_transform = tio.Lambda(random_inverse_transform)
+
+flipping_transforms = tio.OneOf(
+    {
+        tio.Identity(): 0.25,
+        tio.RandomFlip(0, 1): 0.25,
+        tio.RandomFlip(1, 1): 0.25,
+        tio.RandomFlip(2, 1): 0.25,
+    }
+)
+
+# 31% change of not flipping
+# Prob could be changed as a param.
+advanced_flipping = tio.Compose(
+    [
+        tio.RandomFlip(0, 0.31),
+        tio.RandomFlip(1, 0.31),
+        tio.RandomFlip(2, 0.31),
+    ]
+)
 
 
-transform_dict = {
-    # RSD: Write transformations. Use torchvision.io or something.
-}
+basic_transforms = tio.Compose(
+    [
+        tio.RescaleIntensity((0, 1)),
+        flipping_transforms,
+        tio.RandomAffine(scales=1, degrees=(0, 360, 0, 0, 0, 0)),
+    ]
+)
+
+advanced_transforms = tio.Compose(
+    [
+        basic_transforms,
+        random_inverse_transform,
+        tio.RandomElasticDeformation(num_control_points=(3, 3, 3), locked_borders=1),
+        tio.RandomMotion(
+            num_transforms=1, max_displacement=(5, 5, 5), locked_borders=1
+        ),
+        tio.OneOf(
+            {
+                tio.RandomNoise(std=(0, 0.1)): 0.5,
+                tio.RandomBlur(std=(0, 1)): 0.5,
+            }
+        ),
+    ]
+)
 
 
 class Dataset3D(Dataset):
+
+    transforms_dict = {
+        "basic": basic_transforms,
+        "advanced": advanced_transforms,
+    }
+
     def __init__(
         self,
         filename,
@@ -35,35 +90,34 @@ class Dataset3D(Dataset):
     ):
         self.root = os.path.join(img_dir_root, filename)  # Change so h5 is implied
         self.filename = filename
-        self.transform = transform
-        self.target_transform = target_transform
-        # self.data = []
-        # self.targets = []
-        # self._load_data()
-
-    # Not necessary for h5 files
-    # def _load_data(self):
-    #     for file in os.listdir(self.root):
-    #         if file.endswith(".tif"):
-    #             self.data.append(os.path.join(self.root, file))
-    #             self.targets.append(os.path.join(self.root, file))
+        self.transform = Dataset3D.transforms_dict[transform]
+        self.target_transform = Dataset3D.transforms_dict[target_transform]
 
     def __getitem__(self, index):
-        data = torch.from_numpy(
-            np.array(
-                h5py.File(self.root, "r")["noisy3D"][str(index).zfill(5)][
-                    224:-224, 96:-96, 96:-96
-                ]
-            )
-        )
-        # RSD: Temp slicing.
-        target = torch.from_numpy(
-            np.array(
-                h5py.File(self.root, "r")["target3D"][str(index).zfill(5)][
-                    224:-224, 96:-96, 96:-96
-                ]
-            )
-        )
+
+        dimensions = h5py.File(self.root, "r")["noisy3D"][str(index).zfill(5)].shape
+
+        crop_centres = self.random_crop_h5py(dimensions, size=128)
+        # RSD: Hard-code size. Else include hparam in Dataset3D.
+
+        data = self.load_cropped("noisy3D", index, crop_centres, size=128)
+        target = self.load_cropped("target3D", index, crop_centres, size=128)
+
+        # data = torch.from_numpy(
+        #     np.array(
+        #         h5py.File(self.root, "r")["noisy3D"][str(index).zfill(5)][
+        #             224:-224, 96:-96, 96:-96
+        #         ]
+        #     )
+        # )
+        # # RSD: Temp slicing.
+        # target = torch.from_numpy(
+        #     np.array(
+        #         h5py.File(self.root, "r")["target3D"][str(index).zfill(5)][
+        #             224:-224, 96:-96, 96:-96
+        #         ]
+        #     )
+        # )
         if self.transform is not None:
             data = self.transform(data)
         if self.target_transform is not None:
@@ -75,6 +129,52 @@ class Dataset3D(Dataset):
             h5py.File(self.root, "r")["target3D"].keys()
         )  # RSD: Hope this will remain valid and that it works.
 
+    def load_cropped(self, key, index, crop_centres, size):
+        return torch.from_numpy(
+            np.array(
+                h5py.File(self.root, "r")[key][str(index).zfill(5)][
+                    crop_centres[0] - size // 2 : crop_centres[0] + size // 2,
+                    crop_centres[1] - size // 2 : crop_centres[1] + size // 2,
+                    crop_centres[2] - size // 2 : crop_centres[2] + size // 2,
+                ]
+            )
+        )
+
+    def random_crop_h5py(self, dimensions: tuple, size: int, gaussian=True) -> tuple:
+        def find_bounds(dimensions, size):
+            low = -dimensions // 2 + size // 2 + 1
+            high = dimensions // 2 - size // 2 - 1
+            return low, high
+
+        def find_std(dimensions, size):
+            low, high = find_bounds(dimensions, size)
+            return (high - low) * 0.05
+
+        def normal_crop(rng, dimensions, size):
+            return (
+                rng.normal(0, find_std(dimensions, size), 1)
+                .astype(int)
+                .clip(*find_bounds(dimensions, size))
+            )
+
+        rng = np.random.default_rng()
+
+        if gaussian:
+            crop_x = normal_crop(rng, dimensions[0], size)
+            crop_y = normal_crop(rng, dimensions[1], size)
+            crop_z = normal_crop(rng, dimensions[2], size)
+        else:
+            crop_x = rng.uniform(*find_bounds(dimensions[0], size))
+            crop_y = rng.uniform(*find_bounds(dimensions[1], size))
+            crop_z = rng.uniform(*find_bounds(dimensions[2], size))
+
+        crop_x = dimensions[0] // 2 + crop_x
+        crop_y = dimensions[1] // 2 + crop_y
+        crop_z = dimensions[2] // 2 + crop_z
+        # RSD: Hope within bounds
+
+        return crop_x, crop_y, crop_z
+
 
 # class Dataloader3D(DataLoader):
 #     # not necessarily need for an own class
@@ -82,3 +182,38 @@ class Dataset3D(Dataset):
 
 
 # Split data sets
+
+"""
+To make custom transform.
+def custom_transform():
+    return
+
+
+custom_trans = tio.Lambda(custom_transform)
+
+"""
+
+"""
+Ideas to transformations:
+
+Definetly:
+ToTensor --> In the loading of the data. 
+Normalise (0,1)
+Crop --> In the loading of the data. 
+Flip 
+Rotate
+Inverse
+Random Elastic Deformation (Non-linear, weakly)
+
+Perhaps:
+Random Motion
+Random Noise If FDK
+Random Blur? If Iterative
+
+Questions: How to compose?
+Only compose
+One of some?
+
+"""
+
+# Uses one of, could also perform more flips
